6ef0e98dea174d83361da82c17d7db816c9dc66f
mahdishokriyan <mahdi.shokriyan@gmail.com>
2024-10-03 18:39:00 -0400

    adding the jenkins configuration

--- src/test/java/tek/bdd/pages/AccountPage.java
+++ src/test/java/tek/bdd/pages/AccountPage.java
@@ -3,5 +3,6 @@ package tek.bdd.pages;
 import org.openqa.selenium.By;
 
 public class AccountPage {
-    public static final By RECORD_PER_PAGE_DROPDOWN = By.className("chakra-select");
+    public static final By RECORD_PER_PAGE_DROPDOWN =
+            By.xpath("//div[starts-with(@class, 'chakra-select__wrapper')]//child::select");
 }
